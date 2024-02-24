47645ecdafb88a4404552a2e98ee160fd93a4114
Harryson Leal <harrysonleal17@gmail.com>
2024-02-23 20:09:14 -0500

    Cambios  side bar

--- src/pages/Dashboard/componenets/Navbar.jsx
+++ src/pages/Dashboard/componenets/Navbar.jsx
@@ -1,18 +1,23 @@
 import { Link } from "react-router-dom";
+import logo from '../../../assets/img/navbar/logo2.png';
 
 export default function Navbar({ options }) {
     return (
-        <nav className="
-            w-full p-5
-            bg-blue-400
-        ">
-            {options.map(option => 
-                <Link className="
-                    text-white
-                    p-5
-                    hover:bg-blue-800
-                "
-                to={option.url}>{option.option}</Link>)}
-        </nav>
+        <header className="bg-white">
+            <nav className="flex justify-between items-center w-[90%] mx-auto mt-2">
+                <div className="">
+                    <ul className="flex items-center gap-[4vw]">
+                        {options.map(option =>
+                            <li>
+                                <Link className="relative line-animation"
+                                    to={option.url}>{option.option}</Link>
+                            </li>)}
+                    </ul>
+                </div>
+                <div>
+                    <img className="w-[200px]" src={logo} alt="" />
+                </div>
+            </nav>
+        </header>
     );
 }
--- src/pages/Dashboard/componenets/Sidebar.jsx
+++ src/pages/Dashboard/componenets/Sidebar.jsx
@@ -1,10 +1,24 @@
 import { Link } from "react-router-dom";
 
+
+
 export default function Sidebar({ options }) {
+
+    const toggle = () => {
+        document.getElementById('submenu').classList.toggle('hidden');
+        document.getElementById('arrow').classList.toggle('rotate-90');
+    };
+
     return (
-        <div>
-            <div>
-                <span>Clientes</span>
+        <div className="sidebar top-0 bottom-0 lg:left-0 p-2 w-[200px] overflow-y-auto text-center">
+            <div className="p-2.5 flex justify-between rounded-md px-4 duration-300
+               cursor-pointer hover:bg-[#0d5f96] hover:text-white" onClick={toggle}>
+                <span >Clientes</span>
+                <span className="text-sl" id="arrow">
+                    <ion-icon name="chevron-forward-outline"></ion-icon>
+                </span>
+            </div>
+            <div className="text-left text-sm font-thin mt-2 w-4/5 mx-auto hidden" id="submenu">
                 <Link to="/dashboard/createClient">Crear Cliente</Link>
             </div>
             <div>
